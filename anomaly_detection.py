fa4c56b4e1c91b1496614203f21a247639a3f16e
itsMe <61231501+hubert-groen@users.noreply.github.com>
2024-05-25 13:48:04 +0200

    ML - transactions v2

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -1,7 +1,6 @@
 import numpy as np
-import joblib
 
-
+# FIXME: wszystko jest dla jednego usera tylko
 def is_anomalous_transaction(user_id, amount, latitude, longitude, model):
     # Nowa transakcja
     new_transaction = np.array([[amount, latitude, longitude]])
@@ -10,15 +9,4 @@ def is_anomalous_transaction(user_id, amount, latitude, longitude, model):
     is_anomaly = model.predict(new_transaction)[0] == 1
     
     return is_anomaly
-
-
-model = joblib.load('20240525_132030_my_model.pkl')
-
-
-new_transaction = ("user1", 1.0, 52.2296756, 21.0122287)   # to będzie pobrane z kafki zamiast wygenerowane tutaj
-if is_anomalous_transaction(*new_transaction, model):
-    print("Anomalous transaction detected!")
-else:
-    print("Transaction is normal.")
-
 
