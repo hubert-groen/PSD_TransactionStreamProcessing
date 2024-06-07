8ad9dd281c9e6f2d4d9d8083d693fad90a678e57
hubert-groen <hubert.groen@gmail.com>
2024-06-07 12:48:50 +0200

    new add

--- training/add_transaction.py
+++ training/add_transaction.py
@@ -7,17 +7,16 @@ history = TransactionHistory()
 def add_transaction(user_id, amount, latitude, longitude, anomaly):
     history.add_transaction(user_id, amount, latitude, longitude, anomaly)
 
-def generate_random_transactions(num_transactions, user, anomaly_distance_threshold=100):
+
+def generate_random_transactions(num_transactions, user):
     transactions = []
-    base_location = (52.2296756, 21.0122287)  # Baza lokalizacji (np. Warszawa)
+    base_location = (52.2296756, 21.0122287) 
     
     for _ in range(num_transactions):
-        amount = round(random.uniform(50, 500), 2)  # Losowa kwota transakcji między 1 a 500
-        lat = round(random.uniform(50, 54), 6)    # Losowa szerokość geograficzna w Polsce
-        lon = round(random.uniform(19, 23), 6)    # Losowa długość geograficzna w Polsce
-        
-        transaction_location = (lat, lon)
-        distance = geodesic(base_location, transaction_location).km
+
+        amount = round(random.uniform(50, 500), 2) 
+        latitude = round(random.uniform(50, 54), 6)
+        longitude = round(random.uniform(19, 23), 6)
 
         anomaly = 0
 
@@ -28,14 +27,18 @@ def generate_random_transactions(num_transactions, user, anomaly_distance_thresh
         elif rand > 0.3 and rand < 0.4:
             latitude = round(random.uniform(-90, 90), 6)
             longitude = round(random.uniform(-180, 180), 6)
+            while 50 <= latitude <= 54 and 19 <= longitude <= 23:
+                latitude = round(random.uniform(-90, 90), 6)
+                longitude = round(random.uniform(-180, 180), 6)
             anomaly = 1
-        
-        transactions.append((user, amount, lat, lon, anomaly))
+
+        transactions.append((user, amount, latitude, longitude, anomaly))
     
     return transactions
-
 
 transactions = generate_random_transactions(1000, "user1")
 
 for transaction in transactions:
     add_transaction(transaction[0], transaction[1], transaction[2], transaction[3], transaction[4])
+
+
