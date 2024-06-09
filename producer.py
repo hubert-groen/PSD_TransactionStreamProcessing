2b95c4642c282ab569d779d8abcb2543138cad0c
hubert-groen <hubert.groen@gmail.com>
2024-06-09 16:32:49 +0200

    intro mode in generator v2

--- producer.py
+++ producer.py
@@ -20,6 +20,10 @@ if __name__ == '__main__':
 
     for i in range(20):
         transaction = generate_transaction(intro_mode=True)
+        print(f'Producing INTRO message {datetime.now()} | Message = {str(transaction)}')
+        producer_1.send('TOPIC-T3', transaction)
+        time_to_sleep = random.randint(1, 1)
+        time.sleep(time_to_sleep)
         i += 1
 
     while True:
